6f9c506900b2f0a0e8dc37a0c386c36f7c56a73f
Joel Tannas <jtannas@gmail.com>
2024-06-13 17:44:40 -0600

    set up Clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { ClerkProvider } from "@clerk/nextjs";
+import { TopNav } from "./_components/TopNav";
 
 export const metadata = {
   title: "Create T3 App",
@@ -8,25 +10,19 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between bg-purple-500 p-4 text-xl font-semibold border-b-4">
-      <div>Gallery</div>
-    </nav>
-  )
-}
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="flex flex-col gap-4 bg-black text-white">
-        <TopNav />
-        {children}
-      </body>j
-    </html >
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body className="flex flex-col gap-4 bg-black text-white">
+          <TopNav />
+          {children}
+        </body>j
+      </html >
+    </ClerkProvider >
   );
 }
