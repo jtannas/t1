8ff8f0f69f4f12972416ce375077781f43443857
Joel Tannas <jtannas@gmail.com>
2024-06-13 15:06:10 -0600

    set up vercel database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   'https://utfs.io/f/2e6ecdc3-44d7-4503-852d-72eca42ae881-l6jqi8.png',
@@ -11,10 +12,12 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = db.query.posts.findMany()
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {(await posts).map((post) => post.name)}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
