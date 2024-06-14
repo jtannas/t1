27d65f9659985aabc2c28175a91cc54350d5fb6c
Joel Tannas <jtannas@gmail.com>
2024-06-14 17:09:11 -0600

    fix autogenerated sentry config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
+const coreConfig = {
     images: {
         remotePatterns: [
             { hostname: 'utfs.io' },
@@ -18,17 +18,8 @@ const config = {
         ignoreDuringBuilds: true,
     },
 };
-
-export default config;
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
+import { withSentryConfig } from '@sentry/nextjs';
+const config = withSentryConfig(coreConfig, {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
@@ -61,5 +52,5 @@ module.exports = withSentryConfig(
     // https://docs.sentry.io/product/crons/
     // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
-  }
-);
+})
+export default config;
